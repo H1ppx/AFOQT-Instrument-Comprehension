5329b7b71f10bfcf8df799655349e63fd59a1080
Will <willcty@gmail.com>
2021-11-01 22:13:28 -0400

    Storyboard layout complete + Compass base in illustrator

---
+++ AFOQT Instrument Comp/AFOQT Instrument Comp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AFOQT Instrument Comp
+//
+//  Created by Will Chu on 10/28/21.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var artificialHorizonView:SKView!
+    var background = SKSpriteNode(imageNamed: "tmp/ah")
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
